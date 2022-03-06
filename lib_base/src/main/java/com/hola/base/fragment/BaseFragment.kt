78937ec8355b486bfea40d4990d8c0b89737a4cc
Hola <qiaofangchun@qq.com>
2022-03-06 12:43:13 +0800

    init project

--- lib_base/src/main/java/com/hola/base/fragment/BaseFragment.kt
+++ lib_base/src/main/java/com/hola/base/fragment/BaseFragment.kt
@@ -1,13 +1,15 @@
 package com.hola.base.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment(@LayoutRes resId: Int) : Fragment(resId) {
+abstract class BaseFragment : Fragment {
+
+    constructor() : super()
+
+    constructor(@LayoutRes resId: Int) : super(resId)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initWithView()
