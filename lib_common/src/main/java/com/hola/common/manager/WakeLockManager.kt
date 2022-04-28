97de78e54ca4b94f64406eb5f0582625b689ec37
qiaofangchun <qiaofangchun@qq.com>
2022-04-28 23:32:56 +0800

    change arch

--- lib_common/src/main/java/com/hola/common/manager/WakeLockManager.kt
+++ lib_common/src/main/java/com/hola/common/manager/WakeLockManager.kt
@@ -2,26 +2,40 @@ package com.hola.common.manager
 
 import android.annotation.SuppressLint
 import android.os.PowerManager
-import android.os.PowerManager.WakeLock
+import android.util.Log
 import androidx.core.content.ContextCompat
 import com.hola.common.utils.AppHelper
 
 
+@SuppressLint("InvalidWakeLockTag")
 object WakeLockManager {
     private const val TAG = "WakeLockManager"
     private const val TIME_OUT = 5000L
-    private val pm =
+
+    private val mLock by lazy {
         ContextCompat.getSystemService(AppHelper.context, PowerManager::class.java)
+            ?.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
+    }
 
-    @SuppressLint("InvalidWakeLockTag")
-    private val wl = pm?.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
-
-    fun acquire(){
+    fun acquire() {
         acquire(TIME_OUT)
     }
 
-    fun acquire(long: Long){
-        wl?.acquire(long)
+    fun acquire(timeout: Long) {
+        if (mLock == null) {
+            throw IllegalAccessException("The PowerManager or WakeLock is null")
+        }
+        mLock?.takeUnless { it.isHeld }?.acquire(timeout)
     }
 
+    fun release() {
+        release(0)
+    }
+
+    fun release(flags: Int) {
+        if (mLock == null) {
+            throw IllegalAccessException("The PowerManager or WakeLock is null")
+        }
+        mLock?.takeIf { it.isHeld }?.release(flags)
+    }
 }
