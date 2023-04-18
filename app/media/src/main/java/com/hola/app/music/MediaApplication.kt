cdc5a69e3608d3835aac9bd382ea26f40c289f74
wb_qiaofangchun <wb_qiaofangchun@meituan.com>
2023-04-18 14:50:10 +0800

    change arch

--- app/media/src/main/java/com/hola/app/music/MediaApplication.kt
+++ app/media/src/main/java/com/hola/app/music/MediaApplication.kt
@@ -1,8 +1,14 @@
 package com.hola.app.music
 
 import com.hola.base.application.BaseApplication
+import com.hola.media.core.MediaControllerHelper
 
 class MediaApplication : BaseApplication() {
 
     override fun getAppName() = "MediaPlayer"
+
+    override fun onCreate() {
+        super.onCreate()
+        MediaControllerHelper.connectService()
+    }
 }
